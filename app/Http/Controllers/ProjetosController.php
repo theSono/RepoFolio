dca8fb3d154037e7db775d3a89b097618399abc3
ThiagoFamper <thiago.kniphoff@aluno.famper.edu.br>
2023-09-19 18:37:13 -0300

    criação do CRUD no projeto

--- app/Http/Controllers/ProjetosController.php
+++ app/Http/Controllers/ProjetosController.php
@@ -3,150 +3,136 @@
 namespace App\Http\Controllers;
 
 use App\Models\Projeto;
-use App\Http\Controllers\Controller;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
 
-
 class ProjetosController extends Controller
-
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
-    /**
-     * Display a listing of the resource.
-     */
-
 
     public function index()
     {
         $projetos = Projeto::paginate(25);
-        Paginator::useBootstrap();
-
         return view('projeto.lista', compact('projetos'));
-        //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('projeto.formulario');
-        //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $this->validate($request, ['image.*', 'mimes:jpeg, jpg, gif, png']);
-        $pasta = public_path('uploads/images/anexos');
-        if ($request->hasFile('anexos')) {
-            $foto = $request->file('anexos');
-            $miniatura = Image::make($foto->path());
-            $nomeArquivo = $request->file('anexos')->getClientOriginalName();
-            if (!$miniatura->resize(
-                500,
-                500,
-                function ($constraint) {
-                    $constraint->aspectRatio();
-                }
-            )
-                ->save($pasta . '/' . $nomeArquivo)) {
-                $nomeArquivo = "semfoto.jpg";
-            }
-        } else {
-            $nomeArquivo = 'semfoto.jpg';
+        $validator = Validator::make($request->all(), [
+            'anexos' => 'image|mimes:jpeg,jpg,gif,png',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('projeto.create')
+                ->withErrors($validator)
+                ->withInput();
         }
+
+        $nomeArquivo = $this->uploadImagem($request);
 
         $projeto = new Projeto();
         $projeto->fill($request->all());
         $projeto->foto = $nomeArquivo;
+
         if ($projeto->save()) {
-            $request->session()->flash('mensagem_sucesso', "Membro da projeto cadastrado!");
+            Session::flash('mensagem_sucesso', "Projeto cadastrado com sucesso!");
         } else {
-            $request->session()->flash('mensagem_erro', 'Erro ao cadastrar membro da projeto!');
+            Session::flash('mensagem_erro', 'Erro ao cadastrar Projeto!');
         }
-        return Redirect::to('projeto/create');
-        //
+
+        return redirect()->route('projeto.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Projeto $id)
+    public function show(Projeto $projeto)
     {
-        $projetos = Projeto::findOrFail($id);
-        return view('projeto.formulario', compact('projetos'));
-        //
+        return view('projeto.formulario', compact('projeto'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Projeto $projeto)
     {
-        //
+        return view('projeto.editar', compact('projeto'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Projeto $projeto_id)
+    public function update(Request $request, Projeto $projeto)
     {
-        $projeto = Projeto::find($projeto_id);
-        $this->validate($request, ['image.*', 'mimes:jpeg, jpg, gif, png']);
+        $validator = Validator::make($request->all(), [
+            'anexos' => 'image|mimes:jpeg,jpg,gif,png',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('projeto.edit', $projeto->id)
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $nomeArquivo = $this->uploadImagem($request, $projeto->foto);
+
+        $projeto->fill($request->all());
+        $projeto->foto = $nomeArquivo;
+
+        if ($projeto->save()) {
+            Session::flash('mensagem_sucesso', "Projeto atualizado com sucesso!");
+        } else {
+            Session::flash('mensagem_erro', 'Erro ao atualizar Projeto!');
+        }
+
+        return redirect()->route('projeto.show', $projeto->id);
+    }
+
+    public function destroy(Projeto $projeto)
+    {
+        $this->removerImagem($projeto->foto);
+        
+        if ($projeto->delete()) {
+            Session::flash('mensagem_sucesso', 'Projeto removido com sucesso');
+        } else {
+            Session::flash('mensagem_erro', 'Erro ao remover Projeto');
+        }
+
+        return redirect()->route('projeto.index');
+    }
+
+    private function uploadImagem(Request $request, $imagemAtual = null)
+    {
         $pasta = public_path('uploads/images/anexos');
+
         if ($request->hasFile('anexos')) {
             $foto = $request->file('anexos');
             $miniatura = Image::make($foto->path());
-            $nomeArquivo = $request->file('anexos')->getClientOriginalName();
-            if (!$miniatura->resize(
-                500,
-                500,
-                function ($constraint) {
-                    $constraint->aspectRatio();
-                }
-            )
-                ->save($pasta . '/' . $nomeArquivo)) {
+            $nomeArquivo = $foto->getClientOriginalName();
+
+            if (!$miniatura->resize(500, 500, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($pasta . '/' . $nomeArquivo)) {
                 $nomeArquivo = "semfoto.jpg";
             }
-        } else {
-            $nomeArquivo = $projeto->foto;
+
+            // Remover a imagem atual, se houver
+            if ($imagemAtual && $imagemAtual != 'semfoto.jpg') {
+                $this->removerImagem($imagemAtual);
+            }
+
+            return $nomeArquivo;
         }
-        $projeto->fill($request->all());
-        $projeto->foto = $nomeArquivo;
-        if ($projeto->save()) {
-            $request->session()->flash('mensagem_sucesso', "Tipo alterado!");
-        } else {
-            $request->session()->flash('mensagem_erro', 'Deu erro');
-        }
-        return Redirect::to('projeto/' . $projeto->id);
+        return $imagemAtual;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Request $request, Projeto $projeto_id)
+    private function removerImagem($imagem)
     {
-        $projeto = Projeto::findOrFail($projeto_id);
-        $lOk = true;
-        if (!empty($projeto->foto)){
-            if ($projeto->foto != 'semfoto.jpg'){
-                $lOk = unlink(public_path('uploads/images/anexos').$projeto->foto);
-            }
-        }
-        if ($lOk){
-            $projeto->delete();
-            $request->session()->flash('mensagem_sucesso',
-                'Membro da projeto removido com sucesso');
-            return Redirect::to('projeto');
+        $caminhoFoto = public_path('uploads/images/anexos') . '/' . $imagem;
+
+        if (file_exists($caminhoFoto)) {
+            unlink($caminhoFoto);
         }
     }
 }
